66452fd6a313cd80add9d609975901d244cc7b8e
Tawmassino <etomas815@gmail.com>
2023-10-25 22:11:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿namespace L2___Cash_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ===== TO DO =====
+            //get; set - del privatumo
+            //
+
+            #region Guids
+            /*
+             * Guid randomCardNumberGenerator = Guid.NewGuid();
+            List<Guid> randomCardNumbers = new List<Guid>();
+            int howManyUsers = 10;
+            List<Guid> allUsersList = new List<Guid>();
+            for (int i = 0; i < howManyUsers; i++)
+            {
+                allUsersList.Add(randomCardNumberGenerator);
+            }
+
+            allUsersList.ForEach(x => { Console.WriteLine(x); });
+            */
+            #endregion
+
+            // ===== USERS =====
+            //X XX XXX
+            //b69d8293 - 7b62 - 4520 - 93aa - 3ece99882036
+            //8c5eb7d6 - 3ae3 - 452a - b551 - 283fc20d9253
+            //eb311901-4582-481a-b697-13b52e4d2349
+            //c88e180e-b996-40eb-8f38-98abc8db2ba1
+            //f3b5228d-937f-4407-af03-d0bce35ae269
+
+
+
+
+            Console.Clear();
+            //welcome menu
+
+            Console.WriteLine(
+                "╔══════════════════════════════════════════╗" + "\r\n" +
+                "║        WELCOME TO YOUR ATM               ║\r\n" +
+                "║                                          ║\r\n" +
+                "║   1. Show Balance                        ║\r\n" +
+                "║   2. Show Transactions                   ║\r\n" +
+                "║   3. Withdraw                            ║\r\n" +
+                "║   4. Logout                              ║\r\n" +
+                "║   5. Quit                                ║\r\n" +
+                "║                                          ║\r\n" +
+                "║   Please select an option by entering    ║\r\n" +
+                "║   the number, then press [Enter].        ║\r\n" +
+                "║                                          ║\r\n" +
+                "╚══════════════════════════════════════════╝");
+
+
+            // ================================================= END OF MAIN =================================================   
+        }
+        // ===================================================== METHODS =================================================
+
+        #region ColorMethods
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+        //------------meth
+        public static void ColorYellow()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        //------------meth
+        public static void ColorRed()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        //------------meth
+        public static void ColorGreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        //------------meth
+        public static void ResetClr()
+        {
+            Console.ResetColor();
+        }
+        //------------meth
+        #endregion
+
+        // ================================================= END OF METHODS ==============================================
+
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2___Cash_Machine
+{
+    internal class User
+    {
+        //FIELDS
+
+        //PROPERTIES
+
+        public string CardNumber { get; set; }
+        public string PIN { get; set; }
+        public int Balance { get; set; }
+        //public int TransactionHistory { get; set; }
+
+
+        public int DailyLimit { get; set; }
+
+
+
+        //CONSTRUCTORS
+
+        public User(string cardNumber, string pin, int balance)
+        {
+            CardNumber = cardNumber;
+            PIN = pin;
+            Balance = balance;
+        }
+        public User(int transactionHistory, int dailyLimit)
+        {
+            //TransactionHistory = transactionHistory;
+            DailyLimit = dailyLimit;
+        }
+
+        // ======================  METHODS ====================  
+
+        public bool Login()
+        {
+            bool isLoggedIn = false;
+            int attemptsToLogin = 0;
+            int maxAttemptsToLogin = 3;
+
+            do
+            {
+                Console.WriteLine("Please insert your card!");
+                string userCardInput = Console.ReadLine();
+
+                if (userCardInput == CardNumber)
+                {
+                    Console.WriteLine("Enter PIN number:");
+                    string userPin = Console.ReadLine();
+
+                    if (userPin == PIN)
+                    {
+                        isLoggedIn = true;
+                        return isLoggedIn;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect PIN. Please try again.");
+                        attemptsToLogin++;
+                    }
+                }
+                else if (userCardInput == null)
+                {
+                    Console.WriteLine("Please insert your card!");
+                }
+                else
+                {
+                    Console.WriteLine("This card is not registered");
+                    attemptsToLogin++;
+                }
+
+                if (attemptsToLogin >= maxAttemptsToLogin)
+                {
+                    KickOut();
+                    return false;
+                }
+
+            } while (attemptsToLogin < maxAttemptsToLogin);
+
+            return isLoggedIn;
+
+        }
+
+        public void KickOut()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You've been disconnected");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($" The current balance is: {Balance}");
+        }
+
+        public bool DailyLimitReached()
+        {
+            bool isLimitReached = false;
+            string transactionLogFile = "TransactionHistoryLog.txt";
+            DateTime currentDate = DateTime.Now.Date;
+
+            if (File.Exists(transactionLogFile))
+            {
+                // Read the log file
+                string[] logEntries = File.ReadAllLines(transactionLogFile);
+
+                // Calculate the number of transactions for the current day
+                foreach (string logEntry in logEntries)
+                {
+                    if (logEntry.StartsWith(currentDate.ToString("yyyy-MM-dd")))
+                    {
+                        DailyLimit++;
+                    }
+                }
+            }
+            if (DailyLimit == 10)
+            {
+                isLimitReached = true;
+            }
+            return isLimitReached;
+        }
+
+
+        public void Withdraw(int howMuchWithdraw)//ar cia tas pats bool
+        {
+
+            if (howMuchWithdraw <= Balance && DailyLimitReached() == false)
+            {
+                DateTime currentTime = DateTime.Now.Date;
+                string transactionLogFile = "TransactionHistoryLog.txt";
+
+                Console.WriteLine($" Withrawing {howMuchWithdraw} from the Balance of {Balance}");
+                Balance -= howMuchWithdraw;
+                ShowBalance();
+                //TransactionHistory++;
+
+
+                DailyLimit++;
+                //log transaction
+                string logEntry = $"{currentTime:yyyy-MM-dd} - Withdrawal: ${howMuchWithdraw}, Remaining Balance: ${Balance}";
+                // Append the log entry to the transaction history file
+
+                using (StreamWriter writer = File.AppendText(transactionLogFile))
+                {
+                    writer.WriteLine(logEntry);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unable withdraw. Either insufficient balance or daily limit reached.");
+            }
+        }
+
+        public void PrintRecentTransactions()
+        {
+            string logFileName = "TransactionHistoryLog.txt";
+            if (File.Exists(logFileName))
+            {
+                string[] logEntries = File.ReadAllLines(logFileName);
+                int startIndex = logEntries.Length - 5;//imam nuo paskutines vietos #5 israsa
+
+
+                Console.WriteLine(" 5 MOST RECENT TRANSACTIONS: ");
+
+                for (int i = startIndex; i < logEntries.Length; i++)
+                {
+                    Console.WriteLine(logEntries[i]);
+                }
+            }
+        }
+
+        public void Deposit(int howMuchDeposit)
+        {
+
+        }
+
+        // ================== END OF METHODS ==================
+
+    }
+}
